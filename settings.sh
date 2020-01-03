1dfc6379a91cba801e295d46705577d4a6e8ead5
thelamer <ryankuba@gmail.com>
2020-01-02 18:17:38 -0800

    adding mfbsd9 assets

--- settings.sh
+++ settings.sh
@@ -1,3 +1,3 @@
 DOWNLOADS="\
-https://mfsbsd.vx.sk/files/images/8/amd64/mfsbsd-REPLACE_VERSION-RELEASE-amd64.img|default.img
-https://mfsbsd.vx.sk/files/images/8/amd64/mfsbsd-se-REPLACE_VERSION-RELEASE-amd64.img|special-edition.img"
+https://mfsbsd.vx.sk/files/images/9/amd64/mfsbsd-REPLACE_VERSION-RELEASE-amd64.img|default.img
+https://mfsbsd.vx.sk/files/images/9/amd64/mfsbsd-se-REPLACE_VERSION-RELEASE-amd64.img|special-edition.img"
