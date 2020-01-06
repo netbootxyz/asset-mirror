4c4e2fd9e258aa4f5a9ac57f05bb8a13b9701990
thelamer <ryankuba@gmail.com>
2020-01-05 17:34:38 -0800

    need destination

--- settings.sh
+++ settings.sh
@@ -1,2 +1,2 @@
 DOWNLOADS="\
-https://www.advancedclustering.com/wp-content/uploads/2017/02/bootimage-4.26.1-53.tbz2"
+https://www.advancedclustering.com/wp-content/uploads/2017/02/bootimage-4.26.1-53.tbz2|bootimage-4.26.1-53.tbz2"
