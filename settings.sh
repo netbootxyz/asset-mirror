9f014587f8b932b3e6c0824abc0518639d7d7594
thelamer <ryankuba@gmail.com>
2020-01-13 21:19:29 -0800

    adding oracle 7 assets

--- settings.sh
+++ settings.sh
@@ -1,6 +1,6 @@
-URL="http://mirrors.kernel.org/oracle/OL8/REPLACE_VERSION/x86_64/x86_64-boot.iso"
+URL="http://mirrors.kernel.org/oracle/OL7/REPLACE_VERSION/x86_64/x86_64-boot.iso"
 TYPE=file
 CONTENTS="\
 isolinux/initrd.img|initrd
 isolinux/vmlinuz|vmlinuz
-images/install.img|squashfs.img"
+LiveOS/squashfs.img|squashfs.img"
