9100f629d4e18cd6e8a31e7b27ac9b08f0a9f9dc
thelamer <ryankuba@gmail.com>
2020-01-26 12:38:58 -0800

    bad filename

--- settings.sh
+++ settings.sh
@@ -1,5 +1,5 @@
 URL="https://downloads.sourceforge.net/project/linux4m/$(echo REPLACE_VERSION | awk -F'.' '{print $1}').0/livecd/updates/4MLinux-REPLACE_VERSION-64bit.iso"
 TYPE=file
 CONTENTS="\
-boot/bzImage|airootfs.sfs
+boot/bzImage|vmlinuz
 boot/initrd.gz|initrd"
