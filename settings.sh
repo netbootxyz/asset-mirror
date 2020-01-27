17b08d759352171a0bc6bc1bc65227ed703ce18f
thelamer <ryankuba@gmail.com>
2020-01-26 19:54:35 -0800

    adding the SSS assets under 4m umbrella

--- settings.sh
+++ settings.sh
@@ -1,4 +1,4 @@
-URL="https://downloads.sourceforge.net/project/linux4m/$(echo REPLACE_VERSION | awk -F'.' '{print $1}').0/livecd/updates/4MLinux-REPLACE_VERSION-64bit.iso"
+URL="https://downloads.sourceforge.net/project/thesss/TheSSS-REPLACE_VERSION-64bit.iso"
 TYPE=file
 CONTENTS="\
 boot/bzImage|vmlinuz
