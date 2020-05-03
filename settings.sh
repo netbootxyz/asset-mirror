bd20f1387dd5bc4cff163b2b0cb51dbc693d6328
thelamer <ryankuba@gmail.com>
2020-05-03 13:48:01 -0700

    update download paths

--- settings.sh
+++ settings.sh
@@ -1,4 +1,4 @@
 DOWNLOADS="\
-https://hydra.nixos.org/build/REPLACE_VERSION/download/1/bzImage|bzImage
-https://hydra.nixos.org/build/REPLACE_VERSION/download/2/initrd|initrd
-https://hydra.nixos.org/build/REPLACE_VERSION/download/3/netboot.ipxe|netboot.ipxe"
+https://hydra.nixos.org/build/REPLACE_VERSION/download/bzImage|bzImage
+https://hydra.nixos.org/build/REPLACE_VERSION/download/initrd|initrd
+https://hydra.nixos.org/build/REPLACE_VERSION/download/netboot.ipxe|netboot.ipxe"
