9cffd4ab75da6626dd4856fc360e2cf88b412aaf
Antony <antony@mes.ser.li>
2020-10-26 12:56:03 -0500

    Update filestructure for latest version

--- settings.sh
+++ settings.sh
@@ -2,8 +2,8 @@ URL="https://osdn.net/projects/anarchy/storage/anarchy-REPLACE_VERSION-x86_64.is
 TYPE=file
 CONTENTS="\
 arch/x86_64/airootfs.sfs|airootfs.sfs
-arch/boot/x86_64/archiso.img|initrd
-arch/boot/x86_64/vmlinuz|vmlinuz"
+arch/boot/x86_64/initramfs-linux.img|initrd
+arch/boot/x86_64/vmlinuz-linux|vmlinuz"
 EXTRACT_INITRD="true"
 INITRD_NAME="initrd"
 INITRD_TYPE="arch-xz"
