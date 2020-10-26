55904415ce3993af76a2c9d20048fa60fdf14101
Antony <antony@mes.ser.li>
2020-10-26 12:57:41 -0500

    Fix dir structure

--- settings.sh
+++ settings.sh
@@ -1,9 +1,9 @@
 URL="https://osdn.net/projects/anarchy/storage/anarchy-REPLACE_VERSION-x86_64.iso"
 TYPE=file
 CONTENTS="\
-arch/x86_64/airootfs.sfs|airootfs.sfs
-arch/boot/x86_64/initramfs-linux.img|initrd
-arch/boot/x86_64/vmlinuz-linux|vmlinuz"
+anarchy/x86_64/airootfs.sfs|airootfs.sfs
+anarchy/boot/x86_64/initramfs-linux.img|initrd
+anarchy/boot/x86_64/vmlinuz-linux|vmlinuz"
 EXTRACT_INITRD="true"
 INITRD_NAME="initrd"
 INITRD_TYPE="arch-xz"
