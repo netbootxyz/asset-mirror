6e2fa23bc5b16c026d8b59a66dfcfda005ba9cb7
Antony Messerli <antony@mes.ser.li>
2020-11-13 17:20:39 -0600

    boot.iso is not in latest release
    hardsetting for now to get release generated, variation
    in case sentitivity with u2 vs U2 is breaking existing retrieval

--- settings.sh
+++ settings.sh
@@ -1,4 +1,4 @@
-URL="http://mirrors.kernel.org/oracle/OL8/REPLACE_VERSION/x86_64/x86_64-boot.iso"
+URL="http://mirrors.kernel.org/oracle/OL8/REPLACE_VERSION/x86_64/OracleLinux-R8-U2-x86_64-dvd.iso"   
 TYPE=file
 CONTENTS="\
 isolinux/initrd.img|initrd
