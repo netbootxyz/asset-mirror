0826b444b486e15ff3c21a8f70f14259c49b5a9d
Antony Messerli <antony@mes.ser.li>
2020-12-26 11:49:52 -0600

    Tune url var

--- settings.sh
+++ settings.sh
@@ -1,4 +1,4 @@
-URL="https://downloads.sourceforge.net/project/linux4m/$(if [[ \"REPLACE_VERSION\" == *.* ]] ; then echo $(echo REPLACE_VERSION | sed "s/\..*/.0/")/livecd/updates/4MLinux-REPLACE_VERSION-64bit.iso; else echo REPLACE_VERSION/livecd/4MLinux-REPLACE_VERSION-64bit.iso; fi)"
+URL="https://downloads.sourceforge.net/project/linux4m/$(if [[ 'REPLACE_VERSION' != *.0 ]] ; then echo $(echo REPLACE_VERSION | sed "s/\..*/.0/")/livecd/updates/4MLinux-REPLACE_VERSION-64bit.iso; else echo REPLACE_VERSION/livecd/4MLinux-REPLACE_VERSION-64bit.iso; fi)"
 TYPE=file
 CONTENTS="\
 boot/bzImage|vmlinuz
