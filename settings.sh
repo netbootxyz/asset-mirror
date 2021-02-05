d81d96f5b6c0196fb318dcc932d19f31e3ccd7d0
Antony Messerli <antony@mes.ser.li>
2021-02-05 00:10:20 -0600

    Add ovirt

--- settings.sh
+++ settings.sh
@@ -1,6 +1,6 @@
-URL="http://mirrors.kernel.org/oracle/OL8/uREPLACE_VERSION/x86_64/OracleLinux-R8-UREPLACE_VERSION-x86_64-dvd.iso"   
+URL="https://resources.ovirt.org/pub/ovirt-4.4/iso/ovirt-node-ng-installer/REPLACE_VERSION/el8/ovirt-node-ng-installer-REPLACE_VERSION.el8.iso"   
 TYPE=file
 CONTENTS="\
-isolinux/initrd.img|initrd
-isolinux/vmlinuz|vmlinuz
-images/install.img|squashfs.img"
+images/pxeboot/initrd.img|initrd
+images/pxeboot/vmlinuz|vmlinuz
+ovirt-node-ng-image.squashfs.img|squashfs.img"
--- version.sh
+++ version.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-VERSION=$(curl -sL http://mirrors.kernel.org/oracle/OL8/ | awk -F '(href="|/">)' '/="u/ {print $2}' | awk -F'u' '{print $2}'| tail -n 1)
+VERSION=4.4.4-2020122111
 echo "${VERSION}"
