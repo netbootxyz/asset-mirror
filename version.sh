e7f551e0f845952c585ec4614e7ddc5d1efcf49b
Antony Messerli <antony@mes.ser.li>
2021-05-15 15:58:50 -0500

    Add FreeBSD 13

--- version.sh
+++ version.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-VERSION=$(curl -sL https://mfsbsd.vx.sk/files/images/12/amd64/ | grep -Po "(\d+\.)+\d+" | tail -n1)
+VERSION=$(curl -sL https://mfsbsd.vx.sk/files/images/13/amd64/ | grep -Po "(\d+\.)+\d+" | tail -n1)
 echo "${VERSION}"
