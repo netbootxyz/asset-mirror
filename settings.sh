24e3084c1c4004ff82cce5f5308c27f71921fbab
Antony Messerli <antony@mes.ser.li>
2021-08-03 00:50:38 -0500

    Update settings.sh

--- settings.sh
+++ settings.sh
@@ -1,4 +1,4 @@
-URL="https://www.proxmox.com/en/downloads?task=callelement&format=raw&item_id=612&element=f85c494b-2b32-4109-b8c1-083cca2b7db6&method=download&args[0]=512a2af55009dfca32570fe2f05b8c2d"
+URL="https://www.proxmox.com/en/downloads?task=callelement&format=raw&item_id=612&element=f85c494b-2b32-4109-b8c1-083cca2b7db6&method=download&args[0]=5acbfebee9c684db2ddcf6cd42dae403"
 TYPE=file
 CONTENTS="\
 boot/linux26|vmlinuz
