e8a5a48041273bcf13c99d1f47268376976e7055
Antony Messerli <antony@mes.ser.li>
2021-08-31 23:31:47 -0500

    Update settings.sh

--- settings.sh
+++ settings.sh
@@ -2,7 +2,7 @@ URL="https://github.com/endeavouros-team/ISO/releases/download/1-EndeavourOS-ISO
 TYPE=file
 CONTENTS="\
 arch/x86_64/airootfs.sfs|airootfs.sfs
-arch/boot/x86_64/archiso.img|initrd
+arch/boot/x86_64/initramfs-linux.img|initrd
 arch/boot/x86_64/vmlinuz-linux|vmlinuz"
 EXTRACT_INITRD="true"
 INITRD_NAME="initrd"
