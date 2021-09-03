bc6a97be3a35c1868ab9c08f1c3c7a369f43f1e3
Antony Messerli <antony@mes.ser.li>
2021-09-03 12:13:32 -0500

    Switch to direct_file

--- settings.sh
+++ settings.sh
@@ -1,4 +1,2 @@
-URL="https://www.memtest86.com/downloads/memtest86-usb.zip"
-TYPE=file
-CONTENTS="\
-memtest86-usb.img|memtest86-usb.img"
+DOWNLOADS="\
+https://www.memtest86.com/downloads/memtest86-usb.zip|memtest86-usb.img"
