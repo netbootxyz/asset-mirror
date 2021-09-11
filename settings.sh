079d67cf611b47e564a014a2ad952370767d0f3c
Antony Messerli <antony@mes.ser.li>
2021-09-11 10:45:41 -0500

    Add gentoos arm64

--- settings.sh
+++ settings.sh
@@ -1,4 +1,4 @@
-URL="https://www.mirrorservice.org/sites/distfiles.gentoo.org/releases/x86/autobuilds/REPLACE_VERSION/install-x86-minimal-REPLACE_VERSION.iso"
+URL="https://www.mirrorservice.org/sites/distfiles.gentoo.org/releases/arm64/autobuilds/REPLACE_VERSION/install-arm64-minimal-REPLACE_VERSION.iso"
 TYPE=file
 CONTENTS="\
 image.squashfs|image.squashfs
--- version.sh
+++ version.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-VERSION=$(curl -sL http://distfiles.gentoo.org/releases/x86/autobuilds/current-install-x86-minimal/ |awk -F '(install-x86-minimal-|.iso)' '/install-x86-minimal-/ {print $2;exit}')
+VERSION=$(curl -sL http://distfiles.gentoo.org/releases/arm64/autobuilds/current-install-arm64-minimal/ |awk -F '(install-arm64-minimal-|.iso)' '/install-arm64-minimal-/ {print $2;exit}')
 echo "${VERSION}"
