16acedca1c630ed0261d05e6f8f8ce06d0daeee2
Antony Messerli <antony@mes.ser.li>
2021-09-13 17:13:06 -0500

    Fix directory

--- post-processing.sh
+++ post-processing.sh
@@ -3,4 +3,4 @@ set -e
 source settings.sh
 source version.sh
 curl -Lf "${URL}" -o buildout/tails-amd64-${VERSION}.iso
-sha256sum tails-amd64-${VERSION}.iso > buildout/sha256-checksums.txt
+sha256sum buildout/tails-amd64-${VERSION}.iso > buildout/sha256-checksums.txt
