b3ab3ab8e2a1f696315fc2f15208bb8ebe923707
Antony Messerli <antony@mes.ser.li>
2021-09-13 17:18:28 -0500

    Fix buildout tag on sha256

--- post-processing.sh
+++ post-processing.sh
@@ -3,4 +3,6 @@ set -e
 source settings.sh
 source version.sh
 curl -Lf "${URL}" -o buildout/tails-amd64-${VERSION}.iso
-sha256sum buildout/tails-amd64-${VERSION}.iso > buildout/sha256-checksums.txt
+pushd buildout
+sha256sum tails-amd64-${VERSION}.iso > sha256-checksums.txt
+popd
