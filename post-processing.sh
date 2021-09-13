13309e5a0cc87a54168d2e3298b2c1f2b049ea1e
Antony Messerli <antony@mes.ser.li>
2021-09-13 17:28:32 -0500

    Add signature

--- post-processing.sh
+++ post-processing.sh
@@ -3,6 +3,7 @@ set -e
 source settings.sh
 source version.sh
 curl -Lf "${URL}" -o buildout/tails-amd64-${VERSION}.iso
+curl -Lf "${URL}.sig" -o buildout/tails-amd64-${VERSION}.iso.sig
 pushd buildout
 sha256sum tails-amd64-${VERSION}.iso > sha256-checksums.txt
 popd
