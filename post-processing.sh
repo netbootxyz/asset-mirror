759905589908a25a3c71d8f9c552aea8391de07c
Antony Messerli <antony@mes.ser.li>
2021-09-14 17:40:41 -0500

    Add hrmpf release

--- post-processing.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-mkdir -p buildout/tmp
-mount -t msdos -o loop,offset=646656 buildout/default.img buildout/tmp/
-cp buildout/tmp/boot/shredos buildout/
-umount buildout/tmp
-rm buildout/default.img
--- settings.sh
+++ settings.sh
@@ -1,2 +1,7 @@
-DOWNLOADS="\
-https://github.com/PartialVolume/shredos.x86_64/releases/download/v2020.05.011_x86-64_0.30.001/shredos-2020.05.011_x86-64_0.30.001_20210226.img|default.img"
+URL="https://github.com/leahneukirchen/hrmpf/releases/download/vREPLACE_VERSION/hrmpf-x86_64-5.13.15_1-REPLACE_VERSION.iso"
+TYPE=file
+CONTENTS="\
+boot/vmlinuz|vmlinuz
+boot/initrd|initrd
+LiveOS/squashfs.img|squashfs.img"
+
