e4203dfb589e6582a70bf36e358fe2140a564f40
Antony Messerli <antony@mes.ser.li>
2021-09-26 14:11:36 -0500

    Add ShredOS i686

--- settings.sh
+++ settings.sh
@@ -1,2 +1,2 @@
 DOWNLOADS="\
-https://github.com/PartialVolume/shredos.x86_64/releases/download/v2020.05.011_x86-64_0.30.001/shredos-2020.05.011_x86-64_0.30.001_20210226.img|default.img"
+https://github.com/PartialVolume/shredos.i686/releases/download/v2020.02.008_i686-0.30.001/shredos-2020.05.008_i686_0.30.001_20210127.img|default.img"
