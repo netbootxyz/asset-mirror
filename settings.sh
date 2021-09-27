5fcd7e410e07802c22707aba5f610245ef73666d
Antony Messerli <antony@mes.ser.li>
2021-09-27 15:28:44 -0500

    Push archlinux-32 to assets
    Archlinux 32-bit assets are not extracted so creating
    a release for them

--- settings.sh
+++ settings.sh
@@ -1,6 +1,6 @@
-URL="https://www.mirrorservice.org/sites/distfiles.gentoo.org/releases/x86/autobuilds/REPLACE_VERSION/install-x86-minimal-REPLACE_VERSION.iso"
+URL="http://mirror.math.princeton.edu/pub/archlinux32/archisos/archlinux32-REPLACE_VERSION-i686.iso"
 TYPE=file
 CONTENTS="\
-image.squashfs|image.squashfs
-boot/gentoo.igz|initrd
-boot/gentoo|vmlinuz"
+arch/boot/i686/vmlinuz-linux|vmlinuz-linux
+arch/boot/i686/initramfs-linux.img|initramfs-linux.img
+arch/i686/airootfs.sfs|airootfs.sfs"
--- version.sh
+++ version.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 set -e
-VERSION=$(curl -sL http://distfiles.gentoo.org/releases/x86/autobuilds/current-install-x86-minimal/ |awk -F '(install-x86-minimal-|.iso)' '/install-x86-minimal-/ {print $2;exit}')
-echo "${VERSION}"
+VERSION=$(curl -sfL http://mirror.math.princeton.edu/pub/archlinux32/archisos/md5sums | tail -n 1 | awk -F '(archlinux32-|-i686.iso)' '/-i686.iso/ {print $2;exit}')
+# make sure the return has a sane version
+while [[ "${VERSION}" =~ ^[0-9]{4}.[0-9]{2}.[0-9]{2}$ ]]; do
+  echo "${VERSION}"
+  exit 0
+done
+exit 1
