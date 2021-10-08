95710e33d58ca2ad80da7d1f5263d7b4e652351f
Antony Messerli <antony@mes.ser.li>
2021-10-08 14:59:31 -0500

    Add i3wm

--- version.sh
+++ version.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-VERSION=$(curl -sL https://garudalinux.org/downloads.html | awk -F '(garuda-bspwm-linux-zen-|.iso)' '/garuda-bspwm-linux-zen-/ {print $2;exit}')
+VERSION=$(curl -sL https://garudalinux.org/downloads.html | awk -F '(garuda-i3-linux-zen-|.iso)' '/garuda-i3-linux-zen-/ {print $2;exit}')
 echo "${VERSION}"
