e466f982db6e69bcebf3076088aaed838571da63
Antony Messerli <antony@mes.ser.li>
2022-03-07 23:25:55 -0600

    Add proxmox-mailgateway

--- version.sh
+++ version.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-VERSION=$(curl -s http://download.proxmox.com/iso/SHA256SUMS  | grep proxmox-backup-server | tail -n 1 | awk -F'_' {'print $2'} | sed "s/\.iso*$//") 
+VERSION=$(curl -s http://download.proxmox.com/iso/SHA256SUMS  | grep proxmox-mailgateway | tail -n 1 | awk -F'_' {'print $2'} | sed "s/\.iso*$//") 
 echo "${VERSION}"
