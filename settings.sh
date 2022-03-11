bed8dd883d6bc80d34b5a6cf42d297110e15d127
Antony Messerli <antony@mes.ser.li>
2022-03-11 08:37:49 -0600

    Update settings.sh

--- settings.sh
+++ settings.sh
@@ -1,7 +1,6 @@
-URL="https://github.com/leahneukirchen/hrmpf/releases/download/vREPLACE_VERSION/hrmpf-x86_64-5.13.15_1-REPLACE_VERSION.iso"
+URL="https://github.com/leahneukirchen/hrmpf/releases/download/vREPLACE_VERSION/hrmpf-x86_64-5.15.11_1-REPLACE_VERSION.iso"
 TYPE=file
 CONTENTS="\
 boot/vmlinuz|vmlinuz
 boot/initrd|initrd
 LiveOS/squashfs.img|squashfs.img"
-
