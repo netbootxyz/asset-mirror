6b8cc7f88844c9902bd1284edbb1b850e15d93da
Antony Messerli <antony@mes.ser.li>
2022-04-16 19:01:03 -0500

    Adding Tails beta

--- settings.sh
+++ settings.sh
@@ -1,4 +1,4 @@
-URL="http://dl.amnesia.boum.org/tails/stable/tails-amd64-REPLACE_VERSION/tails-amd64-REPLACE_VERSION.iso"
+URL="https://mirrors.edge.kernel.org/tails/alpha/tails-amd64-${VERSION}/tails-amd64-${VERSION}.iso"
 TYPE=file
 CONTENTS="\
 live/vmlinuz|vmlinuz
