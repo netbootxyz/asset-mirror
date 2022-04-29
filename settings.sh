9616af8f0f3cb9e9b86bba2027b35b61b0343a15
Antony Messerli <antony@mes.ser.li>
2022-04-29 08:44:41 -0500

    Fix download

--- settings.sh
+++ settings.sh
@@ -1,2 +1,2 @@
 DOWNLOADS="\
-https://github.com/sideolabs/talos/releases/download/vREPLACE_VERSION/vmlinuz-amd64|initramfs-amd64.xz"
+https://github.com/siderolabs/talos/releases/download/vREPLACE_VERSION/vmlinuz-amd64|initramfs-amd64.xz"
