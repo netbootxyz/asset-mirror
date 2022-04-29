1720fcc22618638e4b8b826f8f4877e0a42adcec
Antony Messerli <antony@mes.ser.li>
2022-04-29 08:40:28 -0500

    Update url

--- version.sh
+++ version.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-VERSION=$(curl --silent "https://api.github.com/repos/talos-systems/talos/releases/latest" | jq -r .tag_name | sed 's/v//')
+VERSION=$(curl --silent "https://api.github.com/repos/sideolabs/talos/releases/latest" | jq -r .tag_name | sed 's/v//')
 echo "${VERSION}"
