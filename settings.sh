8b597a0d99f3bf515a74236e2374eab45c470efa
Antony Messerli <antony@mes.ser.li>
2022-05-27 15:21:26 -0500

    Fix naming to prevent duplicate file entries

--- settings.sh
+++ settings.sh
@@ -1,6 +1,6 @@
 DOWNLOADS="\
-https://releases.rancher.com/harvester/REPLACE_VERSION/harvester-REPLACE_VERSION-vmlinuz-amd64|harvester-REPLACE_VERSION-vmlinuz-amd64
-https://releases.rancher.com/harvester/REPLACE_VERSION/harvester-REPLACE_VERSION-initrd-amd64|harvester-REPLACE_VERSION-initrd-amd64
-https://releases.rancher.com/harvester/REPLACE_VERSION/harvester-REPLACE_VERSION-rootfs-amd64.squashfs|harvester-REPLACE_VERSION-rootfs-amd64.squashfs
-https://releases.rancher.com/harvester/REPLACE_VERSION/harvester-REPLACE_VERSION-amd64.sha512|harvester-REPLACE_VERSION-amd64.sha512
+https://releases.rancher.com/harvester/REPLACE_VERSION/harvester-REPLACE_VERSION-vmlinuz-amd64|harvester-vmlinuz-amd64
+https://releases.rancher.com/harvester/REPLACE_VERSION/harvester-REPLACE_VERSION-initrd-amd64|harvester-initrd-amd64
+https://releases.rancher.com/harvester/REPLACE_VERSION/harvester-REPLACE_VERSION-rootfs-amd64.squashfs|harvester-rootfs-amd64.squashfs
+https://releases.rancher.com/harvester/REPLACE_VERSION/harvester-REPLACE_VERSION-amd64.sha512|harvester-amd64.sha512
 https://releases.rancher.com/harvester/REPLACE_VERSION/version.yaml|version.yaml"
