2c00cc293739772b87566cdb00c5aced589d86f6
Antony Messerli <antony@mes.ser.li>
2022-08-04 21:22:25 -0500

    Add Oracle 9 aarch64

--- settings.sh
+++ settings.sh
@@ -1,4 +1,4 @@
-URL="https://yum.oracle.com/ISOS/OracleLinux/OL9/u0/x86_64/OracleLinux-R9-U0-x86_64-dvd.iso
+URL="https://yum.oracle.com/ISOS/OracleLinux/OL9/u0/aarch64/OracleLinux-R9-U0-aarch64-dvd.iso
 TYPE=file
 CONTENTS="\
 isolinux/initrd.img|initrd
--- version.sh
+++ version.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-VERSION=$(curl -sL https://yum.oracle.com/oracle-linux-isos.html | awk -F '(OracleLinux-|-x86_64-dvd.iso)' '/-x86_64-dvd.iso/ {print $2;exit}')
+VERSION=$(curl -sL https://yum.oracle.com/oracle-linux-isos.html | awk -F '(OracleLinux-|-aarch64-dvd.iso)' '/-aarch64-dvd.iso/ {print $2;exit}')
 echo "${VERSION}"
