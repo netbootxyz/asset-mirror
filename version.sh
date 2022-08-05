5960d29037638ed59668e4ddfdffd2537ae4914c
Antony Messerli <antony@mes.ser.li>
2022-08-04 23:48:58 -0500

    Just use version #

--- version.sh
+++ version.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-VERSION=$(curl -sL https://yum.oracle.com/oracle-linux-isos.html | awk -F '(OracleLinux-|-x86_64-dvd.iso)' '/-x86_64-dvd.iso/ {print $2;exit}')
+VERSION=$(curl -sL https://yum.oracle.com/oracle-linux-isos.html | awk -F '(OracleLinux-|-x86_64-dvd.iso)' '/-x86_64-dvd.iso/ {print $2;exit}' | awk -F'-' {'print $2'})
 echo "${VERSION}"
