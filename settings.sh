cda036425ea1f3b610a6c2be974ee83c6d36bdbe
Antony Messerli <antony@mes.ser.li>
2022-09-17 12:15:56 -0500

    Adjust install files

--- settings.sh
+++ settings.sh
@@ -1,6 +1,6 @@
 URL="https://yum.oracle.com/ISOS/OracleLinux/OL9/u0/aarch64/OracleLinux-R9-U0-aarch64-dvd.iso"
 TYPE=file
 CONTENTS="\
-isolinux/initrd.img|initrd
-isolinux/vmlinuz|vmlinuz
+images/pxeboot/vmlinuz|vmlinuz
+images/pxeboot/initrd.img|initrd
 images/install.img|squashfs.img"
