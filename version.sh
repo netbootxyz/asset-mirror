2ffeed7ffe842dffbf44d13622bd920e33ae9649
Antony Messerli <antony@mes.ser.li>
2022-10-18 00:32:33 -0500

    Add garuda cinnamon

--- version.sh
+++ version.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-VERSION=$(curl -sL https://iso.builds.garudalinux.org/iso/latest/garuda/i3/latest.iso.sha256 | awk -F'-' {'print $5'} | awk -F'.' {'print $1'})
+VERSION=$(curl -sL https://iso.builds.garudalinux.org/iso/latest/garuda/cinnamon/latest.iso.sha256 | awk -F'-' {'print $5'} | awk -F'.' {'print $1'})
 echo "${VERSION}"
