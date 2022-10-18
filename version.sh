e842518c1b3f730946218680db26e332e25136ed
Antony Messerli <antony@mes.ser.li>
2022-10-18 00:19:19 -0500

    Update urls and version checker

--- version.sh
+++ version.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-VERSION=$(curl -sL https://garudalinux.org/downloads.html | awk -F '(garuda-qtile-linux-zen-|.iso)' '/garuda-qtile-linux-zen-/ {print $2;exit}')
+VERSION=$(curl -sL https://iso.builds.garudalinux.org/iso/latest/garuda/qtile/latest.iso.sha256 | awk -F'-' {'print $5'} | awk -F'.' {'print $1'})
 echo "${VERSION}"
