0b83d75ffc3014670886631a417272c9883af261
Antony Messerli <antony@mes.ser.li>
2023-01-05 23:11:05 -0600

    Get latest shredos

--- version.sh
+++ version.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-VERSION=$(curl --silent "https://api.github.com/repos/PartialVolume/shredos.x86_64/releases/latest" | jq -r .tag_name)
+VERSION="0.34_20221231"
 echo "${VERSION}"
