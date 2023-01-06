70627800afffc449de76bad45debbfc2bbc9cebd
Antony Messerli <antony@mes.ser.li>
2023-01-05 22:17:45 -0600

    Update settings.sh

--- settings.sh
+++ settings.sh
@@ -1,4 +1,4 @@
-URL="https://github.com/leahneukirchen/hrmpf/releases/download/vREPLACE_VERSION/hrmpf-x86_64-5.15.11_1-REPLACE_VERSION.iso"
+URL="https://github.com/leahneukirchen/hrmpf/releases/download/vREPLACE_VERSION/hrmpf-x86_64-6.1.3_1-REPLACE_VERSION.iso"
 TYPE=file
 CONTENTS="\
 boot/vmlinuz|vmlinuz
