2016e3dd5f862bddc15965b18484617dccdae4e0
Antony Messerli <antony@mes.ser.li>
2023-03-19 08:49:09 -0500

    Update version checker

--- version.sh
+++ version.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-VERSION=$(curl -sL http://www.system-rescue.org/Download/ | awk -F '(systemrescue-|.iso)' '/systemrescue-/ {print $2; exit}' | cut -d ' ' -f 1)
+VERSION=$(curl -sL http://www.system-rescue.org/Download/ | awk -F '(systemrescue-|.iso)' '/systemrescue-/ {print $2; exit}' | cut -d '-' -f 1)
 echo "${VERSION}"
