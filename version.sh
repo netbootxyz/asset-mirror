95aa72b71a97503117217946962d791e1e5fcb7e
Antony Messerli <antony@mes.ser.li>
2023-07-11 22:22:52 -0500

    Update urls

--- version.sh
+++ version.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-VERSION=$(curl -sL https://endeavouros.com/latest-release/ | awk -F '(EndeavourOS_|.iso)' '/EndeavourOS_/ {print $2; exit}' | cut -d '-' -f 2 )
+VERSION=$(curl -sL https://endeavouros.com/latest-release/ | awk -F '(EndeavourOS_|.iso)' '/EndeavourOS_/ {print $2; exit}' | awk -F'Endeavouros_' {'print $2'})
 echo "${VERSION}"
