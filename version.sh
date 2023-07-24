b0a72d727a63190f696fb980100ab0e7d973d3a1
Antony Messerli <antony@mes.ser.li>
2023-07-23 23:13:53 -0500

    Update version checker

--- version.sh
+++ version.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-VERSION=$(curl -s https://tails.boum.org/install/dvd/index.en.html | awk -F '(/tails-amd64-|.iso)' '/tails-amd64-/ {print $2;exit}')
+VERSION=$(curl -s https://tails.net/install/dvd/index.en.html | awk -F '(/tails-amd64-|.iso)' '/tails-amd64-/ {print $2;exit}')
 echo "${VERSION}"
