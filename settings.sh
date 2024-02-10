f006adb491fd9812e44331decbf67c0f13a2a267
Antony Messerli <antony@mes.ser.li>
2024-02-10 12:13:14 -0600

    Bump shredos version

--- settings.sh
+++ settings.sh
@@ -1,2 +1,2 @@
 DOWNLOADS="\
-https://github.com/PartialVolume/shredos.x86_64/releases/download/v2021.08.2_23_x86-64_0.34/shredos-2021.08.2_23_x86-64_0.34_20221231.img|default.img"
+https://github.com/PartialVolume/shredos.x86_64/releases/download/REPLACE_VERSION/shredos-REPLACE_VERSION_20231110.img|default.img"
--- version.sh
+++ version.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-VERSION="0.34_20221231"
+VERSION=$(curl --silent "https://api.github.com/repos/PartialVolume/shredos.x86_64/releases/latest" | jq -r .tag_name | cut -c2-)
 echo "${VERSION}"
