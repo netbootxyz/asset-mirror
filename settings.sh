0d9393591345510d4610272ffde110bc258c9b67
Antony Messerli <antony@mes.ser.li>
2024-03-17 11:42:40 -0500

    Bump version

--- settings.sh
+++ settings.sh
@@ -1,4 +1,4 @@
-URL="https://github.com/leahneukirchen/hrmpf/releases/download/vREPLACE_VERSION/hrmpf-x86_64-6.1.3_1-REPLACE_VERSION.iso"
+URL="https://github.com/leahneukirchen/hrmpf/releases/download/vREPLACE_VERSION/hrmpf-x86_64-6.5.12_1-REPLACE_VERSION.iso"
 TYPE=file
 CONTENTS="\
 boot/vmlinuz|vmlinuz
