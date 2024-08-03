962a9cb5f48b74f0a97993acfba2122dc8eb00fd
Antony Messerli <antony@mes.ser.li>
2024-08-03 08:53:46 -0500

    Update url

--- settings.sh
+++ settings.sh
@@ -1,4 +1,4 @@
-URL="https://github.com/Vanilla-OS/live-iso/releases/download/2.0/VanillaOS-2-stable.VERSION.iso"
+URL="https://github.com/Vanilla-OS/live-iso/releases/download/2.0/VanillaOS-2-stable.REPLACE_VERSION.iso"
 TYPE=file
 CONTENTS="\
 live/vmlinuz|vmlinuz
