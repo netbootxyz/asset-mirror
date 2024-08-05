0e736968f9810181ccaee103a20fd7ad8e09cae7
Antony Messerli <antony@mes.ser.li>
2024-08-05 01:23:09 -0500

    Update settings

--- settings.sh
+++ settings.sh
@@ -1,4 +1,2 @@
 DOWNLOADS="\
-https://github.com/zbm-dev/zfsbootmenu/releases/download/vREPLACE_VERSION/zfsbootmenu-recovery-x86_64-vREPLACE_VERSION-vmlinuz.EFI|zfsbootmenu-recovery-x86_64.efi"
-DOWNLOADS="\
-https://sourceforge.net/projects/supergrub2/files/REPLACE_VERSION/super_grub2_disk_REPLACE_VERSION/supergrub2-classic-REPLACE_VERSION-x86_64_efi-STANDALONE.EFI/download|supergrub2-classic-x86_64.efi
+https://sourceforge.net/projects/supergrub2/files/REPLACE_VERSION/super_grub2_disk_REPLACE_VERSION/supergrub2-classic-REPLACE_VERSION-x86_64_efi-STANDALONE.EFI/download|supergrub2-classic-x86_64.efi"
