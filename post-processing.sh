911e1aabd4145ce223a1b0d9afa604cdfd350edb
Antony Messerli <antony@mes.ser.li>
2024-08-14 22:14:43 -0500

    Drop version on iso file to prevent duplicates on endpoints.yml

--- post-processing.sh
+++ post-processing.sh
@@ -2,9 +2,9 @@
 set -e
 source settings.sh
 source version.sh
-curl -Lf "${URL}" -o buildout/tails-amd64-${VERSION}.iso
-curl -Lf "${URL}.sig" -o buildout/tails-amd64-${VERSION}.iso.sig
+curl -Lf "${URL}" -o buildout/tails-amd64.iso
+curl -Lf "${URL}.sig" -o buildout/tails-amd64.iso.sig
 cp 9990-misc-helpers.sh buildout/
 pushd buildout
-sha256sum tails-amd64-${VERSION}.iso > sha256-checksums.txt
+sha256sum tails-amd64.iso > sha256-checksums.txt
 popd
