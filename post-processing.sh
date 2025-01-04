bb37edcae1a2125c24b0607584083437dae4e246
Antony Messerli <antony@mes.ser.li>
2025-01-04 11:58:43 -0600

    Pull all binaries into one location

--- post-processing.sh
+++ post-processing.sh
@@ -4,4 +4,7 @@ cd buildout
 sha256sum shellbinpkg.binaries.zip
 unzip shellbinpkg.binaries.zip
 cp ShellBinPkg/UefiShell/X64/Shell.efi uefi-shell-x64.efi
+cp ShellBinPkg/UefiShell/AArch64/Shell.efi uefi-shell-aarch64.efi
+cp ShellBinPkg/UefiShell/Arm/Shell.efi uefi-shell-arm.efi
+rm shellbinpkg.binaries.zip
 sha256sum uefi-shell-*
