3b3d00f28585f85386f2bf241d9170ac508eba57
Antony Messerli <antony@mes.ser.li>
2025-01-26 23:43:26 -0600

    Add kernel version

--- settings.sh
+++ settings.sh
@@ -1,2 +1,2 @@
 DOWNLOADS="\
-https://github.com/zbm-dev/zfsbootmenu/releases/download/vREPLACE_VERSION/zfsbootmenu-recovery-x86_64-vREPLACE_VERSION-vmlinuz.EFI|zfsbootmenu-recovery-x86_64.efi"
+https://github.com/zbm-dev/zfsbootmenu/releases/download/vREPLACE_VERSION/zfsbootmenu-recovery-x86_64-vREPLACE_VERSION-vmlinuz-6.12.11_1.EFI|zfsbootmenu-recovery-x86_64.efi"
