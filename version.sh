e77c558b708b112e743fa0db8645b8145e1533d0
Antony Messerli <antony@mes.ser.li>
2025-03-11 23:29:58 -0500

    Bump version

--- version.sh
+++ version.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-VERSION=$(curl -sL https://endeavouros.com/ | awk -F '(EndeavourOS_Endeavour_neo-|.iso)' '/EndeavourOS_Endeavour_neo-/ {print $2; exit}')
+VERSION=$(curl -sL https://endeavouros.com/ | awk -F '(EndeavourOS_Mercury-|.iso)' '/EndeavourOS_Mercury-/ {print $2; exit}')
 echo "${VERSION}"
