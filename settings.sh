ff74b46aa267e6926e72d7832df57c37f829552a
Antony Messerli <antony@mes.ser.li>
2025-12-05 22:58:03 -0600

    Add proxmox-datacenter-manager

--- settings.sh
+++ settings.sh
@@ -1,4 +1,4 @@
-URL="https://enterprise.proxmox.com/iso/proxmox-ve_REPLACE_VERSION.iso"
+URL="https://enterprise.proxmox.com/iso/proxmox-datacenter-manager_REPLACE_VERSION.iso
 TYPE=file
 CONTENTS="\
 boot/linux26|vmlinuz
--- version.sh
+++ version.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-VERSION=$(curl -s http://download.proxmox.com/iso/SHA256SUMS  | grep proxmox-ve | tail -n 1 | awk -F'_' {'print $2'} | sed "s/\.iso*$//")
+VERSION=$(curl -s http://download.proxmox.com/iso/SHA256SUMS  | grep proxmox-datacenter-manager | tail -n 1 | awk -F'_' {'print $2'} | sed "s/\.iso*$//")
 echo "${VERSION}"
