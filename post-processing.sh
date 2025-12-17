4991621b5ff4a42140e3b19062fec415119bc376
Antony Messerli <antony@mes.ser.li>
2025-12-17 00:13:46 -0600

    Update post-processing.sh

--- post-processing.sh
+++ post-processing.sh
@@ -3,4 +3,7 @@ set -e
 cd buildout
 sha256sum mt86plus.binaries.zip
 unzip mt86plus.binaries.zip
-sha256sum memtest*
+mv mt86p_800_i586 mt86p_i586
+mv mt86p_800_la64 mt86p_la64
+mv mt86p_800_x86_64 mt86p_x86_64
+sha256sum mt86p_*
