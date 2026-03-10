c0e97a1da95bf6bfac1e552e5697767d9e7a731f
Antony Messerli <antony@mes.ser.li>
2026-03-10 00:58:57 -0500

    Tweaking for downloads

---
+++ pre-processing.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+
+mkdir -p buildout
+curl -sfL -A "Mozilla/5.0 (X11; Linux x86_64)" \
+  https://www.memtest86.com/downloads/memtest86-usb.zip \
+  -o /tmp/memtest86-usb.zip
--- settings.sh
+++ settings.sh
@@ -1,2 +1,2 @@
 DOWNLOADS="\
-https://www.memtest86.com/downloads/memtest86-usb.zip|memtest86-usb.zip"
+file:///tmp/memtest86-usb.zip|memtest86-usb.zip"
