0c956f6889dc0df0ebccedb299da5c91a531b921
Antony Messerli <antony@mes.ser.li>
2026-03-10 00:55:13 -0500

    Fix version scraper

--- version.sh
+++ version.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-VERSION=$(curl -sfL https://www.memtest86.com/download.htm | grep Free | awk -F '(Version | Build)' '/ Build/ {print $2;exit}')
+VERSION=$(curl -sfL -A "Mozilla/5.0 (X11; Linux x86_64)" https://www.memtest86.com/download.htm | grep Free | awk -F '(Version | Build)' '/ Build/ {print $2;exit}')
 echo "${VERSION}"
