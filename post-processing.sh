bf7a6bdf9a73b73ac54153891cb9d0b1a53bcc1c
Antony Messerli <antony@mes.ser.li>
2026-03-10 00:46:55 -0500

    Change copied file from shredos to bzimage

--- post-processing.sh
+++ post-processing.sh
@@ -4,6 +4,6 @@ set -e
 
 mkdir -p buildout/tmp
 mount -t msdos -o loop,offset=646656 buildout/default.img buildout/tmp/
-cp buildout/tmp/boot/shredos buildout/
+cp buildout/tmp/boot/bzimage buildout/shredos
 umount buildout/tmp
 rm buildout/default.img
