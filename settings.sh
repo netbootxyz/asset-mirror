be8d32c33d069e3b724c47c13c0b74e71fa3bc11
Antony Messerli <antony@mes.ser.li>
2026-03-10 00:26:13 -0500

    Update download link to new version of shredos

--- settings.sh
+++ settings.sh
@@ -1,2 +1,2 @@
 DOWNLOADS="\
-https://github.com/PartialVolume/shredos.x86_64/releases/download/vREPLACE_VERSION/shredos-REPLACE_VERSION_20250123.img|default.img"
+https://github.com/PartialVolume/shredos.x86_64/releases/download/vREPLACE_VERSION/shredos-REPLACE_VERSION_20260204.img|default.img"
