e6b193ffa80172db6092a67bba7ea0856a6bb56d
Antony Messerli <antony@mes.ser.li>
2026-05-20 18:48:48 -0500

    Update settings.sh

--- settings.sh
+++ settings.sh
@@ -1,3 +1,3 @@
 DOWNLOADS="\
-https://github.com/netbootxyz/ubuntu-spins/releases/download/0.0.2/vmlinuz|vmlinuz
+https://github.com/netbootxyz/ubuntu-spins/releases/download/REPLACE_VERSION/vmlinuz|vmlinuz
 https://github.com/netbootxyz/ubuntu-spins/releases/download/REPLACE_VERSION/initrd|initrd"
